1d093312937b04e51be8075cbdb43e9e485ae78c
gaogzhen <gaogzhen@foxmail.com>
2023-04-02 18:12:51 +0800

    动态初始化列表-TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 // 创建外壳组件App
-import {Component} from 'react'
+import { Component } from 'react'
+import Header from './components/Header';
+import List from './components/List';
+import Footer from './components/Footer';
+import './App.css'
 
 class App extends Component {
+  state = {todos:[
+    {id: 1, name: '晨练', done: false},
+    {id: 2, name: '学习spring', done: true},
+    {id: 3, name: '逛街', done: true},
+    {id: 4, name: '晚饭', done: false},
+  ]}
   render() {
+    const {todos} = this.state
     return (
-      <div>
-        App
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List todos={todos}/>
+          <Footer />
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // 引入react-dom
 import ReactDOM from 'react-dom/client'
 // 引入App组件
-import App from '../02_hello-react/App'
+import App from './App'
 
 // 渲染组件到页面
 const root = ReactDOM.createRoot(document.getElementById('root'))
