357efc86582df50c53b8264593b3f1ce10a55642
gaogzhen <gaogzhen@foxmail.com>
2023-04-03 17:33:31 +0800

    添加一个todo-TodoList-react应用

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,31 @@ import Footer from './components/Footer';
 import './App.css'
 
 class App extends Component {
+
+  /**
+   * 待办事项列表
+   */
   state = {todos:[
-    {id: 1, name: '晨练', done: false},
-    {id: 2, name: '学习spring', done: true},
-    {id: 3, name: '逛街', done: true},
-    {id: 4, name: '晚饭', done: false},
+    {id: '001', name: '晨练', done: false},
+    {id: '002', name: '学习spring', done: true},
+    {id: '003', name: '逛街', done: true},
+    {id: '004', name: '晚饭', done: false},
   ]}
+
+  /**
+   * 添加待办事项
+   * @param {*} todo 待办事项
+   */
+  addTodo = (todo) => {
+    this.setState({todos: [todo, ...this.state.todos]})
+  }
+
   render() {
     const {todos} = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header />
+          <Header addTodo={this.addTodo}/>
           <List todos={todos}/>
           <Footer />
         </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { nanoid } from "nanoid";
 import './index.css'
 
 export default class Header extends Component {
+
+  handleKeyUp = (event) => {
+    const {keyCode, target} = event
+    // 判断输入enter获取值
+    if (keyCode !== 13) {
+      return
+    }
+    // 校验数据，非空
+    const val = target.value.trim() || target.value
+    if (!val) {
+      alert('待做事项不能为空！')
+      return
+    }
+    // 子组件给父组件传值，通过函数调用
+    // 构建todo对象
+    const todo = {id: nanoid(), name: val, done: false}
+    this.props.addTodo(todo)
+    // 清空输入框
+    target.value = ''
+  }
+
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" />
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" onKeyUp={this.handleKeyUp} />
       </div>
     )
   }
