79344140ec60919287c889a426d8e0971bc45afd
gaogzhen <gaogzhen@foxmail.com>
2023-05-31 16:42:22 +0800

    0604嵌套路由与路由传参-react路由-react

---
+++ 14_src_0604嵌套路由与路由传参/index.js
@@ -0,0 +1,18 @@
+// 引入react核心库
+import React from "react";
+// 引入react-dom
+import ReactDOM from 'react-dom/client'
+// 引入路由器
+import { BrowserRouter } from "react-router-dom";
+// 引入App组件
+import App from './App'
+
+// 渲染组件到页面
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
